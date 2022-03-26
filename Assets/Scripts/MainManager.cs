7ed17e556021eccb9b397fc7009ba14a236be1f3
Benjamin Bonny <benjamin.bonny@gmail.com>
2022-03-26 16:08:33 +0100

    Add menu and data persistency

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,3 +1,5 @@
+using System.IO;
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,71 +8,54 @@ using UnityEngine.UI;
 
 public class MainManager : MonoBehaviour
 {
-    public Brick BrickPrefab;
-    public int LineCount = 6;
-    public Rigidbody Ball;
 
-    public Text ScoreText;
-    public GameObject GameOverText;
-    
-    private bool m_Started = false;
-    private int m_Points;
-    
-    private bool m_GameOver = false;
+    public static MainManager Instance;
 
-    
-    // Start is called before the first frame update
-    void Start()
+    public string PlayerPseudo;
+    public string BestScorePseudo;
+    public int BestScorePoints;
+
+    private void Awake()
     {
-        const float step = 0.6f;
-        int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
-        for (int i = 0; i < LineCount; ++i)
+        if (Instance != null)
         {
-            for (int x = 0; x < perLine; ++x)
-            {
-                Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
-                var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
-                brick.PointValue = pointCountArray[i];
-                brick.onDestroyed.AddListener(AddPoint);
-            }
+            Destroy(gameObject);
+            return;
         }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        LoadBestScore();
+        PlayerPseudo = "AAA";
+    }
+    [System.Serializable]
+    class SaveData
+    {
+        public int BestScorePoints;
+        public string BestScorePseudo;
     }
 
-    private void Update()
+    public void SaveBestScore()
     {
-        if (!m_Started)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                m_Started = true;
-                float randomDirection = Random.Range(-1.0f, 1.0f);
-                Vector3 forceDir = new Vector3(randomDirection, 1, 0);
-                forceDir.Normalize();
+        SaveData data = new SaveData();
+        data.BestScorePoints = BestScorePoints;
+        data.BestScorePseudo = BestScorePseudo;
 
-                Ball.transform.SetParent(null);
-                Ball.AddForce(forceDir * 2.0f, ForceMode.VelocityChange);
-            }
-        }
-        else if (m_GameOver)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-            }
-        }
+        string json = JsonUtility.ToJson(data);
+    
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
     }
 
-    void AddPoint(int point)
+    public void LoadBestScore()
     {
-        m_Points += point;
-        ScoreText.text = $"Score : {m_Points}";
-    }
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
 
-    public void GameOver()
-    {
-        m_GameOver = true;
-        GameOverText.SetActive(true);
+            BestScorePoints = data.BestScorePoints;
+            BestScorePseudo = data.BestScorePseudo;
+        }
     }
 }
